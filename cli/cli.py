64ea7980961874bb612dfcfe8fe60b24702dbde9
José Barros <jose.pedro@didimo.co>
2022-04-20 11:25:24 +0100

    INI-2004 As a user, I should be able to check how many points a generation will consume before they are consumed

--- cli/cli.py
+++ cli/cli.py
@@ -151,12 +151,15 @@ def account(config, raw):
 @click.option('--package-type', '-p', multiple=True,
               type=click.Choice(["fbx", "gltf"]),
               help="Specify output types for this didimo. This flag can be used multiple times.", show_default=True)
+@click.option('--ignore-cost', is_flag=True,
+              default=False,
+              help="Do not prompt user to confirm operation cost")
 @click.option("--version", "-v",
               type=click.Choice(["2.5"]),
               default="2.5",
               help="Version of the didimo.", show_default=True)
 @pass_api
-def new(config, type, input, depth, feature, max_texture, no_download, no_wait, output, package_type, version):
+def new(config, type, input, depth, feature, max_texture, no_download, no_wait, output, package_type, version, ignore_cost):
     """
     Create a didimo
 
@@ -201,6 +204,20 @@ def new(config, type, input, depth, feature, max_texture, no_download, no_wait, 
 
     for feature_item in feature:
         payload[feature_item] = 'true'
+
+    if not ignore_cost:    
+        # check how many points a generation will consume before they are consumed 
+        # and prompt user to confirm operation before proceeding with the didimo generation request
+        r = http_post_withphoto(url+"-cost", config.access_key, payload, input, depth)
+        is_error = r.json()['is_error']
+        if is_error:
+            click.echo("The requested configuration is invalid! Aborting...")
+            exit(1);
+
+        estimated_cost = r.json()['cost']
+        click.echo("The cost of this operation is: "+str(estimated_cost))
+        click.confirm('Are you sure you want to proceed with the didimo creation?', abort=True)
+        click.echo("Proceeding...")
 
     r = http_post_withphoto(url, config.access_key, payload, input, depth)
 
